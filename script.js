44b4e3ecd7f13fd105bbb4e3fbbe55f862c81f39
pratham rameshwar <121504583+pratham145@users.noreply.github.com>
2023-08-16 01:51:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+var rn;
+var  score=0;
+function makebubbles(){
+    let clutter = ""
+    for(let i=0;i<=132;i++){
+        
+        let rn = Math.floor(Math.random()*5)+1
+        clutter += `  <div class="bubble" >${rn}</div>`
+        document.querySelector(".pbtm").innerHTML = clutter
+    }
+}
+
+
+function runtime(){
+   var timer = 61;
+    var  timerr;
+    setInterval(
+timerr =  function(){
+if(timer>0){
+timer--;
+document.querySelector(".time").textContent = timer;
+}
+else{
+    clearInterval(timerr)
+   document.querySelector(".pbtm").innerHTML = `<h1>Game Over!
+   Press any key to restart the game</h1>.`
+   
+}
+    },1000)
+}
+
+function hit(){
+    rn = Math.floor(Math.random()*5)+1
+    document.querySelector(".hit").textContent = rn
+ }
+
+
+function increasescore(){
+ 
+    score += 10
+    document.querySelector(".score").textContent = score
+    console.log(score.textContent)
+  
+}
+
+document.querySelector(".pbtm").addEventListener("click",(a)=>{
+    var clicked =Number(a.target.textContent)
+  if(clicked == rn){
+    
+    increasescore();
+makebubbles();
+hit();
+  }
+})
+
+makebubbles();
+runtime();
+hit();
